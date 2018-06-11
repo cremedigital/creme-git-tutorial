8d7fdec8e2d7180f9afa8afa4dd9c84abbc0e783
Henrik Söderlund <github@henriksoderlund.com>
2018-06-11 11:00:56 +0800

    removed comments and cleaned up code

--- pin-propaganda-vulcanppost_article_leadform.php
+++ pin-propaganda-vulcanppost_article_leadform.php
@@ -1,28 +1,14 @@
 
-<!–– CSS Styles for Propaganda articles ––>
-<style>
-    /* Inline CSS styles goes here - not needed for normal Propaganda articles, but can be edited */
-</style>
-
-<!–– This line adds support for ajax form animation and form submission to creme server ––>
 <script async type='text/javascript' src='/wp-content/plugins/creme-digital/assets/js/formtools.js'></script>
 
-<!–– PROPAGANDA LEADFORM ––>
 <div class="cdp-leadform">
     <div class="cdp-form-container">
         <div id="mc_embed_signup">
             <form id="propaganda-form" action="https://propaganda.cremedigital.com/forms/process.php?callback=?" method="post" target="_blank">
                 
-                <!–– EDIT - Information text inside the form - edit according to client materials ––>
                 <div id="mc_embed_signup_scroll">
                     <h2>If you'd like to learn more about the solution, drop your details below and we'll reach out to you.</h2>
                     <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-
-                <!–– EDIT - Lead Form Fields ––>
-                <!–– EDIT - duplicate field blocks according to lead form fields created on propaganda cremedigital com forms ––>
-                <!–– Don't edit classes ––>
-                <!–– 'for' and 'id' have to be identical ––>
-                <!–– don't edit type="text" ––>
 
                     <div class="mc-field-group">
                         <label for="FNAME">First Name <span class="asterisk">*</span></label>
@@ -62,13 +48,11 @@
                         <?php endif; ?>
                     </div>
 
-                    <!–– SUBMIT BUTTON TEXT: EDIT 'value', 'name' if requested by client ––>
                     <div class="clear">
                         <input type="submit" value="Submit" name="submit" id="mc-embedded-subscribe" class="button">
                     </div>
                 </div>
             </form>
-
             
             <div class="thank-you" style="display: none;">
             <!–– Thank-you message text in animation: EDIT if requested by client ––>
